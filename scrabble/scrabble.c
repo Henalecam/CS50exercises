e89d5fef21e1c792d61ccf8195e3e19d17e22617
Henrique Alexandre <henriquealexandredec@gmail.com>
2022-11-13 21:36:37 -0300

    Update scrabble.c

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,7 +2,12 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
+/*
+Receive word input
+   (65) a = 0 = A (97)
+   (66) b = 1 = B (98)
+   (90) z = 2 = Z (122)
+*/
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
@@ -19,9 +24,43 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else if (score1 == score2)
+    {
+        printf("Tie!\n");
+    }
+    else
+    {
+        printf("Somenthing is wrong\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int score = 0;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (word[i] >= 65 && word[i] <= 90) // Ascii a - z
+        {
+            score += POINTS[word[i] - 65];
+        }
+        else if (word[i] >= 97 && word[i] <= 122) // Ascii A - Z
+        {
+            score += POINTS[word[i] - 97];
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return score;
 }
